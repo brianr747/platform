84b1bd90fc2e35a51f32156a8d414757664b4b4f
brianr747 <brianr747@gmail.com>
2022-01-12 13:13:22 -0500

    DB.nomics database changed, updated to new series.

--- examples/ex20190503_second_fetch.py
+++ examples/ex20190503_second_fetch.py
@@ -6,5 +6,5 @@ Plot the 10-year US Treasury/Euro area AAA govvie  spread in 4 -- count'em, 4 --
 from econ_platform.start import fetch, quick_plot
 
 ust10 = fetch('F@DGS10')
-euro_AAA_10 = fetch('D@Eurostat/irt_euryld_d/D.EA.PYC_RT.Y10.CGB_EA_AAA')
-quick_plot(ust10-euro_AAA_10, title='U.S. 10Y Spread Over AAA-Rated Euro Govvie')
+euro_AAA_10 = fetch('D@Eurostat/irt_h_euryld_d/D.PAR.Y10.EA')
+quick_plot(100*(ust10-euro_AAA_10), title='U.S. 10Y Spread Over Euro Govvie')
